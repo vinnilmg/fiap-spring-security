fbced335a42cf7c418ae54a016531e086cfe75e4
= <vini.delimagomes@gmail.com>
2025-01-16 19:09:47 -0300

    feat: init proj

---
+++ src/main/java/com/fiap/spring_security_demo/SpringSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package com.fiap.spring_security_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/spring_security_demo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.spring_security_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
